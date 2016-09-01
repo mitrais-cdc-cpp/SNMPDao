f909a6ab12dcd7a711b8ef8ed49f0f6615e55bf8
albertusadityo <didithere@gmail.com>
2016-09-01 10:44:36 +0700

    added dml script for snmpobjecttype table

---
+++ bat/dml_snmpobjecttype.sql
@@ -0,0 +1,19 @@
+INSERT INTO `nemstadb`.`snmpobjecttype`
+	(`TypeName`)
+VALUES
+	("string");
+
+INSERT INTO `nemstadb`.`snmpobjecttype`
+	(`TypeName`)
+VALUES
+	("int");
+	
+INSERT INTO `nemstadb`.`snmpobjecttype`
+	(`TypeName`)
+VALUES
+	("double");
+	
+INSERT INTO `nemstadb`.`snmpobjecttype`
+	(`TypeName`)
+VALUES
+	("float");
