020a4b587da2afa9419193d6e4a4fd378be5bfc9
suarkadipa <suarkadipa@gmail.com>
2016-09-14 14:35:31 +0800

    changed all "nemstadb" schema names into "nemsta"

--- bat/snmpdao_create_script_mssql.sql
+++ bat/snmpdao_create_script_mssql.sql
@@ -1,31 +1,31 @@
 USE [master]
 GO
 
-CREATE DATABASE [nemstadb]
+CREATE DATABASE [nemsta]
 GO
 
-USE [nemstadb]
+USE [nemsta]
 GO
 
 /* 
  * Before we want to use a schema, we have to create it first!
- * Create nemstadb schema
+ * Create nemsta schema
  */
 IF NOT EXISTS (
 SELECT  schema_name
 FROM    information_schema.schemata
-WHERE   schema_name = 'nemstadb' ) 
+WHERE   schema_name = 'nemsta' ) 
 
 BEGIN
-EXEC sp_executesql N'CREATE SCHEMA nemstadb'
+EXEC sp_executesql N'CREATE SCHEMA nemsta'
 END
 
-/****** Object:  Table [nemstadb].[itemizedobject]    Script Date: 8/31/2016 2:00:09 PM ******/
+/****** Object:  Table [nemsta].[itemizedobject]    Script Date: 8/31/2016 2:00:09 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [nemstadb].[itemizedobject](
+CREATE TABLE [nemsta].[itemizedobject](
 	[ItemizedObjectId] [int] IDENTITY(1,1) NOT NULL,
 	[NetworkElementFk] [int] NOT NULL,
 	[SnmpObjectFk] [int] NOT NULL,
@@ -38,12 +38,12 @@ CREATE TABLE [nemstadb].[itemizedobject](
 ) ON [PRIMARY]
 
 GO
-/****** Object:  Table [nemstadb].[monitorhistory]    Script Date: 8/31/2016 2:00:09 PM ******/
+/****** Object:  Table [nemsta].[monitorhistory]    Script Date: 8/31/2016 2:00:09 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [nemstadb].[monitorhistory](
+CREATE TABLE [nemsta].[monitorhistory](
 	[MonitorHistoryId] [int] IDENTITY(1,1) NOT NULL,
 	[LastUpdate] [datetime2](0) NOT NULL,
 	[Note] [nvarchar](45) NULL,
@@ -56,12 +56,12 @@ CREATE TABLE [nemstadb].[monitorhistory](
 ) ON [PRIMARY]
 
 GO
-/****** Object:  Table [nemstadb].[networkelement]    Script Date: 8/31/2016 2:00:09 PM ******/
+/****** Object:  Table [nemsta].[networkelement]    Script Date: 8/31/2016 2:00:09 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [nemstadb].[networkelement](
+CREATE TABLE [nemsta].[networkelement](
 	[NetworkElementId] [int] IDENTITY(1,1) NOT NULL,
 	[ElementName] [nvarchar](45) NOT NULL,
 	[MacAddress] [nvarchar](45) NOT NULL,
@@ -77,12 +77,12 @@ CREATE TABLE [nemstadb].[networkelement](
 ) ON [PRIMARY]
 
 GO
-/****** Object:  Table [nemstadb].[snmpobject]    Script Date: 8/31/2016 2:00:09 PM ******/
+/****** Object:  Table [nemsta].[snmpobject]    Script Date: 8/31/2016 2:00:09 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [nemstadb].[snmpobject](
+CREATE TABLE [nemsta].[snmpobject](
 	[SnmpObjectId] [int] IDENTITY(1,1) NOT NULL,
 	[Mib] [nvarchar](45) NOT NULL,
 	[Oid] [nvarchar](45) NOT NULL,
@@ -94,12 +94,12 @@ CREATE TABLE [nemstadb].[snmpobject](
 ) ON [PRIMARY]
 
 GO
-/****** Object:  Table [nemstadb].[snmpobjecttype]    Script Date: 8/31/2016 2:00:09 PM ******/
+/****** Object:  Table [nemsta].[snmpobjecttype]    Script Date: 8/31/2016 2:00:09 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [nemstadb].[snmpobjecttype](
+CREATE TABLE [nemsta].[snmpobjecttype](
 	[SnmpObjectTypeId] [int] IDENTITY(1,1) NOT NULL,
 	[TypeName] [nvarchar](45) NOT NULL,
 	[SnmpObjectValueFk] [int] NOT NULL,
@@ -111,12 +111,12 @@ CREATE TABLE [nemstadb].[snmpobjecttype](
 ) ON [PRIMARY]
 
 GO
-/****** Object:  Table [nemstadb].[snmpobjectvalue]    Script Date: 8/31/2016 2:00:09 PM ******/
+/****** Object:  Table [nemsta].[snmpobjectvalue]    Script Date: 8/31/2016 2:00:09 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [nemstadb].[snmpobjectvalue](
+CREATE TABLE [nemsta].[snmpobjectvalue](
 	[SnmpObjectValueId] [int] IDENTITY(1,1) NOT NULL,
 	[Value] [nvarchar](45) NOT NULL,
 	[SnmpObjectFk] [int] NOT NULL,
@@ -129,56 +129,56 @@ CREATE TABLE [nemstadb].[snmpobjectvalue](
 
 GO
 /****** Object:  Index [fk_ItemizedObject_NetworkElement_idx]    Script Date: 8/31/2016 2:00:09 PM ******/
-CREATE NONCLUSTERED INDEX [fk_ItemizedObject_NetworkElement_idx] ON [nemstadb].[itemizedobject]
+CREATE NONCLUSTERED INDEX [fk_ItemizedObject_NetworkElement_idx] ON [nemsta].[itemizedobject]
 (
 	[NetworkElementFk] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 /****** Object:  Index [fk_ItemizedObject_SnmpObject1_idx]    Script Date: 8/31/2016 2:00:09 PM ******/
-CREATE NONCLUSTERED INDEX [fk_ItemizedObject_SnmpObject1_idx] ON [nemstadb].[itemizedobject]
+CREATE NONCLUSTERED INDEX [fk_ItemizedObject_SnmpObject1_idx] ON [nemsta].[itemizedobject]
 (
 	[SnmpObjectFk] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 /****** Object:  Index [fk_MonitorHistory_SnmpObjectValue1_idx]    Script Date: 8/31/2016 2:00:09 PM ******/
-CREATE NONCLUSTERED INDEX [fk_MonitorHistory_SnmpObjectValue1_idx] ON [nemstadb].[monitorhistory]
+CREATE NONCLUSTERED INDEX [fk_MonitorHistory_SnmpObjectValue1_idx] ON [nemsta].[monitorhistory]
 (
 	[SnmpObjectValueFk] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 /****** Object:  Index [fk_SnmpObjectType_SnmpObjectValue1_idx]    Script Date: 8/31/2016 2:00:09 PM ******/
-CREATE NONCLUSTERED INDEX [fk_SnmpObjectType_SnmpObjectValue1_idx] ON [nemstadb].[snmpobjecttype]
+CREATE NONCLUSTERED INDEX [fk_SnmpObjectType_SnmpObjectValue1_idx] ON [nemsta].[snmpobjecttype]
 (
 	[SnmpObjectValueFk] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 /****** Object:  Index [fk_SnmpObjectValue_SnmpObject1_idx]    Script Date: 8/31/2016 2:00:09 PM ******/
-CREATE NONCLUSTERED INDEX [fk_SnmpObjectValue_SnmpObject1_idx] ON [nemstadb].[snmpobjectvalue]
+CREATE NONCLUSTERED INDEX [fk_SnmpObjectValue_SnmpObject1_idx] ON [nemsta].[snmpobjectvalue]
 (
 	[SnmpObjectFk] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
-ALTER TABLE [nemstadb].[monitorhistory] ADD  DEFAULT (NULL) FOR [Note]
+ALTER TABLE [nemsta].[monitorhistory] ADD  DEFAULT (NULL) FOR [Note]
 GO
-ALTER TABLE [nemstadb].[snmpobject] ADD  DEFAULT (NULL) FOR [ObjectName]
+ALTER TABLE [nemsta].[snmpobject] ADD  DEFAULT (NULL) FOR [ObjectName]
 GO
-ALTER TABLE [nemstadb].[itemizedobject]  WITH CHECK ADD  CONSTRAINT [itemizedobject$fk_ItemizedObject_NetworkElement] FOREIGN KEY([NetworkElementFk])
-REFERENCES [nemstadb].[networkelement] ([NetworkElementId])
+ALTER TABLE [nemsta].[itemizedobject]  WITH CHECK ADD  CONSTRAINT [itemizedobject$fk_ItemizedObject_NetworkElement] FOREIGN KEY([NetworkElementFk])
+REFERENCES [nemsta].[networkelement] ([NetworkElementId])
 GO
-ALTER TABLE [nemstadb].[itemizedobject] CHECK CONSTRAINT [itemizedobject$fk_ItemizedObject_NetworkElement]
+ALTER TABLE [nemsta].[itemizedobject] CHECK CONSTRAINT [itemizedobject$fk_ItemizedObject_NetworkElement]
 GO
-ALTER TABLE [nemstadb].[itemizedobject]  WITH CHECK ADD  CONSTRAINT [itemizedobject$fk_ItemizedObject_SnmpObject1] FOREIGN KEY([SnmpObjectFk])
-REFERENCES [nemstadb].[snmpobject] ([SnmpObjectId])
+ALTER TABLE [nemsta].[itemizedobject]  WITH CHECK ADD  CONSTRAINT [itemizedobject$fk_ItemizedObject_SnmpObject1] FOREIGN KEY([SnmpObjectFk])
+REFERENCES [nemsta].[snmpobject] ([SnmpObjectId])
 GO
-ALTER TABLE [nemstadb].[itemizedobject] CHECK CONSTRAINT [itemizedobject$fk_ItemizedObject_SnmpObject1]
+ALTER TABLE [nemsta].[itemizedobject] CHECK CONSTRAINT [itemizedobject$fk_ItemizedObject_SnmpObject1]
 GO
-ALTER TABLE [nemstadb].[snmpobjectvalue]  WITH CHECK ADD  CONSTRAINT [snmpobjectvalue$fk_SnmpObjectValue_SnmpObject1] FOREIGN KEY([SnmpObjectFk])
-REFERENCES [nemstadb].[snmpobject] ([SnmpObjectId])
+ALTER TABLE [nemsta].[snmpobjectvalue]  WITH CHECK ADD  CONSTRAINT [snmpobjectvalue$fk_SnmpObjectValue_SnmpObject1] FOREIGN KEY([SnmpObjectFk])
+REFERENCES [nemsta].[snmpobject] ([SnmpObjectId])
 GO
-ALTER TABLE [nemstadb].[snmpobjectvalue] CHECK CONSTRAINT [snmpobjectvalue$fk_SnmpObjectValue_SnmpObject1]
+ALTER TABLE [nemsta].[snmpobjectvalue] CHECK CONSTRAINT [snmpobjectvalue$fk_SnmpObjectValue_SnmpObject1]
 GO
 
 USE [master]
 GO
-ALTER DATABASE [nemstadb] SET  READ_WRITE 
+ALTER DATABASE [nemsta] SET  READ_WRITE 
 GO
