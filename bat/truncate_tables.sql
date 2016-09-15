49220eae6387a0cacc30dbc1722eaadf798428e4
albertusadityo <didithere@gmail.com>
2016-09-15 08:47:52 +0700

    added truncate script for tables

---
+++ bat/truncate_tables.sql
@@ -0,0 +1,6 @@
+TRUNCATE `nemstadb`.`snmpobject`;
+TRUNCATE `nemstadb`.`itemizedobject`;
+TRUNCATE `nemstadb`.`monitorhistory`;
+TRUNCATE `nemstadb`.`networkelement`;
+TRUNCATE `nemstadb`.`snmpobjecttype`;
+TRUNCATE `nemstadb`.`snmpobjectvalue`;
